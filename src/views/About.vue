255bbbcb439c0c164e6165440a52ec3d3fed61fe
deathsent <thanat.p38@gmail.com>
2020-09-18 13:48:39 +0700

    commit

---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>Example</h1>
+    <button id="count" @click="count++">Count++</button>
+    <button @click="onClick()">Count--</button><br>
+    <span>{{count}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"abount",
+  data() {
+    return {
+      count:0
+    }
+  },
+  methods:{
+    onClick(){
+      this.count=this.count - 1
+    }
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
